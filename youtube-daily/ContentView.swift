3e7af42bd081381b83b1f06123125c415cdaf590
Nicholas Alderman <nicholas@Nicholass-MacBook-Pro-4.local>
2021-01-16 13:19:13 -0500

    First Commit

---
+++ youtube-daily/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  youtube-daily
+//
+//  Created by Nicholas Alderman on 2021-01-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ youtube-daily/youtube_dailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtube_dailyApp.swift
+//  youtube-daily
+//
+//  Created by Nicholas Alderman on 2021-01-16.
+//
+
+import SwiftUI
+
+@main
+struct youtube_dailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
